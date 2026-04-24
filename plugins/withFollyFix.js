bec68c48b056f32d907937285ed4b2d7965b014e
SOULGPT <fakemillionaire980@gmail.com>
2026-04-24 18:03:05 +0200

    fix: restore improved Folly fix for Xcode compatibility

--- plugins/withFollyFix.js
+++ plugins/withFollyFix.js
@@ -15,14 +15,13 @@ const withFollyFix = (config) => {
       if ['RCT-Folly', 'RNReanimated', 'React-Core', 'React-RCTFabric'].include?(target.name)
         target.build_configurations.each do |config|
           config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FOLLY_CFG_NO_COROUTINES'
+          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FOLLY_HAS_COROUTINES=0'
           
-          config.build_settings['HEADER_SEARCH_PATHS'] = '$(inherited) ' +
-            '"$(PODS_ROOT)/Headers/Public/RCT-Folly" ' +
-            '"$(PODS_ROOT)/Headers/Public/DoubleConversion" ' +
-            '"$(PODS_ROOT)/Headers/Public/glog" ' +
-            '"$(PODS_ROOT)/Headers/Public/fmt" ' +
-            '"$(PODS_TARGET_SRCROOT)"'
+          config.build_settings['HEADER_SEARCH_PATHS'] ||= '$(inherited)'
+          config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/RCT-Folly"'
+          config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/DoubleConversion"'
+          config.build_settings['HEADER_SEARCH_PATHS'] << ' "$(PODS_ROOT)/Headers/Public/glog"'
+
         end
       end
     end
